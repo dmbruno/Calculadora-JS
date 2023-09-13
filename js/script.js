3a8a875b12c6d6dce2738293185d8f0838b3473a
diego <dmbruno61@gmail.com>
2023-09-13 11:22:28 -0300

    calculadora terminada

--- js/script.js
+++ js/script.js
@@ -1,41 +1,4 @@
-/* 
-let edad = 19
-let edadString = parseInt(prompt("Ingresa tu edad"))
 
-if (edadString >= 18 && edadString <= 35) {
-    console.log("podes comprar una birrita");
-} else if(edadString >= 35){
-    console.log("vaya a dormir");
-}
-else {
-    console.log("Usted es menor de edad, No puede comprar alcohol");
-    
-}
- */
-/* 
-let edadUsuario = parseInt(prompt("Ingresa tu edad"))
-
-
-if (edadUsuario <= 18){
-    console.log("sos muy chivo para entrar");
-}else if (edadUsuario >= 18 && edadUsuario <= 35) {
-    console.log("podes pasar");
-}else if (edadUsuario >= 35 && edadUsuario <= 69) {
-    console.log("Usted debe dormir");
-}
- */
-
-function agregarALaPantalla(value) {
-    document.getElementById('pantalla').value += value;
-}
-function calcular() {
-    const valorPantalla = document.getElementById('pantalla').value;
-    const result = eval(valorPantalla);
-    document.getElementById('pantalla').value = result;
-}
-function limpiarPantalla() {
-    document.getElementById('pantalla').value = '';
-} 
 //esto agrega a la pantalla el valor del boton que toquemos en la calculadora
 function agregar (valor){
     document.getElementById('pantalla').value += valor
